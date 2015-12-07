11ab2616c6cf8da8da0ed3f7769d74edc98d3077
nabijaczleweli <nabijaczleweli@gmail.com>
2015-12-07 16:04:48 +0100

    Trivial client, only sends stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::net::TcpStream;
+use std::io::{Read, Write};
+
 fn main() {
-    println!("Hello, world!");
+	let mut strm = TcpStream::connect("127.0.0.1:50030").ok().unwrap();
+	println!("{:?}", strm);
+
+	println!("{:?}", strm.write(&[0x41]));
+	println!("{:?}", strm.flush());
+
+	let mut rr = String::new();
+	println!("{:?}", strm.read_to_string(&mut rr).unwrap());
+	println!("{}", rr);
 }
