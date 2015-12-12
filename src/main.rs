d4c316cf9dc21fc3365ba3e1731deb3d61b2d2aa
nabijaczleweli <nabijaczleweli@gmail.com>
2015-12-12 18:57:56 +0100

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-extern crate chattium_oxide_client;
 extern crate chattium_oxide_lib;
 extern crate hyper;
 
@@ -10,16 +9,12 @@ use chattium_oxide_lib::json::ToJsonnable;
 fn main() {
 	let client = Client::new();
 
-	let body =
-		match ChatMessage::new(ChatUser::me("nabijaczleweli".to_string()), "Nooble sucks".to_string()).to_json_string() {
-			Ok(json) => json,
-			Err(error) => {
-				println!("Couldn't serialize message: {}", error);
-				return;
+	match ChatMessage::new(ChatUser::me("nabijaczleweli".to_string()), "Nooble sucks".to_string()).to_json_string() {
+		Ok(json) =>
+			match client.post("http://127.0.0.1:50030").body(&*&json).send() {
+				Ok(response) => println!("Server responded with status {}", response.status),
+				Err(error) => println!("POSTing the message failed: {}", error),
 			},
-		};
-	match client.post("http://127.0.0.1:50030").body(&*&body).send() {
-		Ok(response) => println!("Server responded with status {}", response.status),
-		Err(error) => println!("POSTing the message failed: {}", error),
-	}
+		Err(error) => println!("Couldn't serialize message: {}", error),
+	};
 }
