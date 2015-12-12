bc6327e09be3cdc702b8f2a20096b749724187ea
nabijaczleweli <nabijaczleweli@gmail.com>
2015-12-12 18:54:51 +0100

    Semi-functional client, sends a message to server at 50030. Add more fields to Cargo.toml

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
+extern crate chattium_oxide_client;
+extern crate chattium_oxide_lib;
 extern crate hyper;
 
 use hyper::client::Client;
+use chattium_oxide_lib::{ChatMessage, ChatUser};
+use chattium_oxide_lib::json::ToJsonnable;
 
 
 fn main() {
 	let client = Client::new();
 
-	match client.post("http://127.0.0.1:50030").body("BODY").send() {
+	let body =
+		match ChatMessage::new(ChatUser::me("nabijaczleweli".to_string()), "Nooble sucks".to_string()).to_json_string() {
+			Ok(json) => json,
+			Err(error) => {
+				println!("Couldn't serialize message: {}", error);
+				return;
+			},
+		};
+	match client.post("http://127.0.0.1:50030").body(&*&body).send() {
 		Ok(response) => println!("Server responded with status {}", response.status),
-		Err(error) => println!("Error {}", error),
+		Err(error) => println!("POSTing the message failed: {}", error),
 	}
 }
