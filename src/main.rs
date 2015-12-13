f661fa6729e764be6514e2ece2ea7630af06c348
nabijaczleweli <nabijaczleweli@gmail.com>
2015-12-13 17:10:12 +0100

    Types! Types!

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,9 @@ fn main() {
 	let client = Client::new();
 	let options = Options::parse();
 
-	match ChatMessage::new(ChatUser::me(options.name), "Noobel sucks".to_string()).to_json_string() {
+	match ChatMessage::new(ChatUser::me(options.name.clone()), "Noobel sucks".to_string()).to_json_string() {
 		Ok(json) =>
-			match client.post(options.server).body(&*&json).send() {
+			match client.post(&*&options.server).body(&*&json).send() {
 				Ok(response) => println!("Server responded with status {}", response.status),
 				Err(error) => println!("POSTing the message failed: {}", error),
 			},
