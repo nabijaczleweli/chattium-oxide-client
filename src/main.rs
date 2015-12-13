5cd267fd6c9078fd7f6155c2fa784514594a191b
nabijaczleweli <nabijaczleweli@gmail.com>
2015-12-13 16:46:22 +0100

    Add Options and a parser therefor. Use Options for connecting

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
+extern crate chattium_oxide_client;
 extern crate chattium_oxide_lib;
 extern crate hyper;
 
 use hyper::client::Client;
 use chattium_oxide_lib::{ChatMessage, ChatUser};
 use chattium_oxide_lib::json::ToJsonnable;
+use chattium_oxide_client::Options;
 
 
 fn main() {
 	let client = Client::new();
+	let options = Options::parse();
 
-	match ChatMessage::new(ChatUser::me("nabijaczleweli".to_string()), "Nooble sucks".to_string()).to_json_string() {
+	match ChatMessage::new(ChatUser::me(options.name), "Noobel sucks".to_string()).to_json_string() {
 		Ok(json) =>
-			match client.post("http://127.0.0.1:50030").body(&*&json).send() {
+			match client.post(options.server).body(&*&json).send() {
 				Ok(response) => println!("Server responded with status {}", response.status),
 				Err(error) => println!("POSTing the message failed: {}", error),
 			},
