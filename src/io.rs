432d0310605ef0942cf6c5812d263db41235fad4
nabijaczleweli <nabijaczleweli@gmail.com>
2016-01-13 15:34:04 +0100

    Remove io::read_*

--- src/io.rs
+++ src/io.rs
@@ -2,19 +2,6 @@ use std::fmt::Arguments as FormatArguments;
 use std::io::{self, Write};
 
 
-pub fn read_prompted(prompt: FormatArguments) -> io::Result<Option<String>> {
-	try!(io::stdout().write_fmt(prompt));
-	try!(io::stdout().flush());
-
-	read_unprompted()
-}
-
-pub fn read_unprompted() -> io::Result<Option<String>> {
-	let mut obuf = String::new();
-	try!(io::stdin().read_line(&mut obuf));  // Don't match on this directly, because it returns with "\r\n"
-	Ok(maybe_trimmed(obuf))
-}
-
 pub fn maybe_trimmed(buf: String) -> Option<String> {
 	let buf = buf.trim();
 	match buf.len() {
